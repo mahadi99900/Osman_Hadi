4a3b5c14b13d80c47203f31b4005d88da389ba61
Mahdi bin Iqbal <151855877+mahadihasan9@users.noreply.github.com>
2026-02-21 22:45:14 +0600

    Remove commented lines from start_archive function

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ def start_archive():
     print("Welcome to Shaheed Osman Bin Hadi Digital Archive")
     print("================================================")
     
-    # ১. সাহায্য বা কমান্ড লিস্ট দেখানো
+    
     oh.help()
     
-    # ২. ছবি বা আর্ট দেখানো
+   
     print("\nDisplaying Portrait...")
     oh.his_face1()
     
-    # ৩. বাংলা ও ইংরেজি উভয় ভাষায় তথ্য দেখা
+   
     print("\n--- Biography / জীবনী ---")
     oh.bio()
     
